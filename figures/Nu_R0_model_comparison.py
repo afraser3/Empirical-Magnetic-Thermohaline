e42ac7c6320ab4aff576cf603e6929c4085e1041
Evan Anders <evanhanders@gmail.com>
2022-03-17 12:26:06 -0500

    small improvements to figure colors / labels

--- figures/Nu_R0_model_comparison.py
+++ figures/Nu_R0_model_comparison.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import optimize as opt
 from matplotlib import pyplot as plt
+from palettable.colorbrewer.qualitative import Dark2_4
 plt.style.use('apj.mplstyle')
 
 
@@ -258,17 +259,19 @@ rs = rfromR(R0s, tau)
 #plt.semilogy(R0s, NuC_Kippenhahn_Cp1-1, label=r'Kippenhahn model, $\alpha_{\rm{th}} = 8$')
 #plt.semilogy(R0s, NuC_Kippenhahn_Cp2-1, label=r'Kippenhahn model, $\alpha_{\rm{th}} = 700$')
 #plt.xlim((10.0, 1.0/tau))
-plt.semilogy(rs, NuC_Traxler-1, label='Traxler model')
-plt.semilogy(rs, NuC_Brown-1, label='Brown model')
-plt.semilogy(rs, NuC_Kippenhahn_Cp1-1, label=r'Kippenhahn model, $\alpha_{\rm{th}} = 8$')
-plt.semilogy(rs, NuC_Kippenhahn_Cp2-1, label=r'Kippenhahn model, $\alpha_{\rm{th}} = 700$')
-plt.semilogy(rs, NuCs_HG19_HB1-1, label=r'HG19 model, $H_B = 10^{-11}$')
+plt.semilogy(rs, NuC_Traxler-1, c=Dark2_4.mpl_colors[0], label='Traxler')
+plt.semilogy(rs, NuC_Brown-1,  c=Dark2_4.mpl_colors[1], label='Brown')
+plt.semilogy(rs, NuC_Kippenhahn_Cp1-1, c=Dark2_4.mpl_colors[3], label=r'Kippenhahn')
+plt.semilogy(rs, NuC_Kippenhahn_Cp2-1, c=Dark2_4.mpl_colors[3])
+plt.semilogy(rs, NuCs_HG19_HB1-1, c=Dark2_4.mpl_colors[2], label=r'HG19 ($H_B = 10^{-11}$)', lw=3, zorder=1)
 #plt.semilogy(rs, NuCs_HG19_HB2-1, label=r'HG19 model, $H_B = 1$')
 plt.xlim(0, 1)
 plt.xlabel(r'$r$')
 plt.ylabel(r'$D_{\rm{th}}/\kappa_\mu$')
-#plt.ylim(1e-4, 1e6)
-plt.legend(fontsize=6, frameon=False)
+plt.ylim(1e-4, 1e8)
+plt.legend(fontsize=8, frameon=False, ncol=2)
+plt.text(x=0.99, y=NuC_Kippenhahn_Cp2[-1]*3, s=r'$\alpha_{\rm{th}} = 700$', color=Dark2_4.mpl_colors[3], fontsize=8, ha='right')
+plt.text(x=0.99, y=NuC_Kippenhahn_Cp1[-1]/8, s=r'$\alpha_{\rm{th}} = 8$', color=Dark2_4.mpl_colors[3], fontsize=8, ha='right')
 plt.savefig('Nu_models_comparison.pdf', bbox_inches='tight', dpi=400)
 plt.savefig('Nu_models_comparison.png', bbox_inches='tight', dpi=400)
 #plt.show()
