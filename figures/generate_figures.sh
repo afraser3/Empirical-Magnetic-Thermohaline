7609d3b5fc0dfcf5ae84f17c883eafa4a5b93ad6
Evan Anders <evanhanders@gmail.com>
2022-04-14 18:58:29 -0500

    adds generate_figures.sh

---
+++ figures/generate_figures.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 Nu_R0_model_comparison.py
+python3 publication_mesa_r.py
+python3 publication_mixing_vs_r.py
+python3 publication_punchline.py
+python3 publication_resolution_test.py
